dfcaf0f92247ff6db777d51fbf18bdd8f69bda8e
Akamine <kalaspa@gmail.com>
2016-02-25 21:07:22 +0100

    First commit, creation of the project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sywocClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sywocClientApp
+ */
+angular.module('sywocClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sywocClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sywocClientApp
+ */
+angular.module('sywocClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
